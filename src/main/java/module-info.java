dbb58ede420ae04b368dda13099111ffef18f2b1
Haseeb Ul Hassan <100109683+Haseeeb1@users.noreply.github.com>
2023-04-18 10:40:25 +0500

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.todolist {
     requires javafx.controls;
     requires javafx.fxml;
     requires mail;
-    
     opens com.todolist to javafx.fxml;
     opens MainCode to javafx.base;
     exports com.todolist;
